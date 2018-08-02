cc98becc33e499de88a9e3a471365dafecfe63bb
Daniel <dpeterson@basecampcodingacademy.org>
2018-08-02 13:59:59 -0500

    appended 'x' and '/'

--- bowling.py
+++ bowling.py
@@ -1,25 +1,41 @@
 def new_main():
-    score_times = []
+    total_throws = []
+    frame_throws = []
     name = input("Player's name: ")
     for frame in range(1, 10):
         print_frame(frame)
         first_throw = input_first_throw(name)
         if first_throw == 10:
             print('AWESOME.... You got a strike!!')
-            score_times.append(first_throw)
-            print(score_times)
+            total_throws.append(total_throws[0])
+            total_throws.append(first_throw)
+            frame_throws.append('X')
+            total = sum(total_throws)
+            print(total)
+            print(total_throws)
+            print(frame_throws)
         else:
             second_throw = input_second_throw(first_throw)
             if first_throw + second_throw == 10:
+                total_balls = first_throw + second_throw
                 print('YAY, you got a SPARE')
-                score_times.append(second_throw)
-                print(score_times)
+                total_throws.append(second_throw)
+                total_throws.append(total_balls)
+                frame_throws.append('/')
+                total = sum(total_throws)
+                print(total)
+                print(total_throws)
+                print(frame_throws)
             else:
-                print('You bowled great!')
+                print('\nYou are doing a great job!')
                 bowling_total = first_throw + second_throw
-                score_times.append(bowling_total)
-                total = sum(score_times)
-                print(score_times)
+                total_throws.append(bowling_total)
+                frame_throws.append(second_throw)
+                frame_throws.append(first_throw)
+                total = sum(total_throws)
+                print(frame_throws)
+                print(total_throws)
+                print(total)
     print('You have a total of', total)
     print('\nGoodbye', name)
 
@@ -31,10 +47,6 @@ def input_first_throw(name):
         if score > 10:
             print('There is only Ten Pins!!')
         else:
-            print(
-                '\nKeep Bowling, its still your turn',
-                name,
-            )
             return score
 
 
@@ -52,7 +64,6 @@ def input_second_throw(first_throw):
 
 
 def print_frame(frame):
-    print('---------------------------------------------------------')
     print('Frame: ', frame)
     print('---------------------------------------------------------')
 
