5907c97cfbd3f9e9fc2261e5a8d05ae39dca01a3
Daniel <dpeterson@basecampcodingacademy.org>
2018-08-03 09:08:43 -0500

    Changed some stuff

--- bowling.py
+++ bowling.py
@@ -1,17 +1,4 @@
-# for i in range(len(frame_throws)):
-
-#     if '10' in frame_throws[i]:
-#         ball_1 = frame_throws[i][0]
-#         # total += ball_1
-#         if i + 1 < len(frame_throws):
-#             next_ball = frame_throws[i + 1][0][1]
-#             total = total + next_ball
-#         else:
-#             total = total + sum(frame_throws[i])
-#         total_throws.append(next_ball)
-
-
-def new_main():
+def worker_function():
     total_throws = []
     frame_throws = []
     name = input("Player's name: ")
@@ -20,45 +7,47 @@ def new_main():
         first_throw = input_first_throw(name)
         if first_throw == 10:
             print('AWESOME.... You got a strike!!')
+            total_balls = first_throw + second_throw
             total_throws.append(first_throw)
+            total_throws.append(total_balls)
             frame_throws.append('X')
             total = sum(total_throws)
-            print(total)
-            # print(total_throws)
             print(frame_throws)
+            print(total)
+
         else:
             second_throw = input_second_throw(first_throw)
             if first_throw + second_throw == 10:
-                total_balls = first_throw + second_throw
+
                 print('WOW, you got a SPARE!!')
                 total_throws.append(second_throw)
-                total_throws.append(total_balls)
+                total_throws.append(first_throw)
                 frame_throws.append('/')
                 total = sum(total_throws)
-                print(total)
-                # print(total_throws)
                 print(frame_throws)
+                print(total)
+
             else:
                 print('\nYou are doing a great job!')
                 bowling_total = first_throw + second_throw
                 total_throws.append(bowling_total)
                 frame_throws.append([second_throw, first_throw])
                 total = sum(total_throws)
-                print(total)
-                # print(total_throws)
                 print(frame_throws)
+                print(total)
+
     print('You have a total of', total)
     print('\nThanks for playing', name, 'have a blessed day!')
 
 
 def input_first_throw(name):
     while True:
-        score = int(input('\nHow many pins did you knock down? '))
+        first_throw = int(input('\nHow many pins did you knock down? '))
 
-        if score > 10:
+        if first_throw > 10:
             print('There is only Ten Pins quit cheating!!')
         else:
-            return score
+            return first_throw
 
 
 def input_second_throw(first_throw):
@@ -76,8 +65,8 @@ def input_second_throw(first_throw):
 
 def print_frame(frame):
     print('Frame: ', frame)
-    print('---------------------------------------------------------')
+    print('*------------------------------------------------------------*')
 
 
 if __name__ == '__main__':
-    new_main()
+    worker_function()
